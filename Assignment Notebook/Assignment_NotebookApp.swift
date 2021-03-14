b9149e70e04be69f82955b7f8362600a19bf08e7
Declan Smith <dmansmith03@yahoo.com>
2021-03-14 14:54:35 -0500

    Initial Commit

---
+++ Assignment Notebook/Assignment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_NotebookApp.swift
+//  Assignment Notebook
+//
+//  Created by Declan Smith on 3/14/21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
